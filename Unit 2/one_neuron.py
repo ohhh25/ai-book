9f6f5347465c4c50cb42193c14620e0e2c038caf
Lukas Cao <lukascao06@gmail.com>
2021-11-21 10:42:29 -0800

    Update one_neuron.py

--- Unit 2/one_neuron.py
+++ Unit 2/one_neuron.py
@@ -1,10 +1,39 @@
-import numpy as np
-np.random.seed(0)
+print("\n")
 
-print(np.random.randint(low=-10, high=10))    # X
-print(np.random.randint(low=-10, high=10))    # w
-print(np.random.randint(low=-10, high=10))    # b
+import numpy as np
+X = np.array([2, 0, -7])    # 3 input features, vector
+def one_neuron(X):    # multiple input features
+    w = np.array([5, -2, -10])
+    b = -10
+    return np.sum(w * X) + b    
+
+print(one_neuron(X))
 
 # =========================================================
-np.random.seed(2)
-print(np.random.randint(low=-10, high=10, size=3))    # remaining Xs
+# Multiple Examples
+
+Xs = np.array([[2, 0, -7],
+              [-2, 9, 0],
+              [5, 1, -1],
+              [3, 0, -4]])
+def one_neuron(Xs):    # multiple input features, multiple examples
+    w = np.array([5, -2, -10])
+    b = -10
+    return np.sum(w * Xs, axis=1) + b
+
+print(one_neuron(Xs))
+
+# =========================================================
+# Without Summation Function
+Xs = np.array([[2, 0, -7],
+              [-2, 9, 0],
+              [5, 1, -1],
+              [3, 0, -4]])
+def one_neuron(Xs):    # multiple input features, multiple examples
+    w = np.array([5, -2, -10])
+    b = -10
+    return np.dot(Xs, w) + b
+
+print(one_neuron(Xs))
+
+print("\n")
