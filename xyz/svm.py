01a6b3d8c5894f40f8e17ec958482ab670bdf534
Lukas Cao <lukascao06@gmail.com>
2021-11-20 16:39:15 -0800

    xyz

---
+++ xyz/svm.py
@@ -0,0 +1,17 @@
+import numpy as np
+np.random.seed(0)
+
+print(np.round_(np.random.uniform(low=-10.0), decimals=3))
+print(np.round_(np.random.randn(4), decimals=3))
+print(np.round_(np.random.randn(4, 3), decimals=3))
+
+# =========================================================
+scaler = -3.963
+
+vector = [0.742, 15.33, -2.268, 1.334]
+
+matrix = [[-0.843, 1.97, 1.266],
+          [-0.506, 2.545, 1.081],
+          [0.484, 0.579, -0.18],
+          [1.41, -0.374, 0.275]]
+
