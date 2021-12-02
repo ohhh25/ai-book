cdb59795aebce3a732bef7d540d3a302eb5c3bdf
Lukas Cao <lukascao06@gmail.com>
2021-12-01 20:49:28 -0800

    Update multiple_neurons.py

--- Unit 2/multiple_neurons.py
+++ Unit 2/multiple_neurons.py
@@ -10,4 +10,20 @@ def multiple_neurons(X):    # multiple input features
 
 print(multiple_neurons(X))
 
+# =========================================================
+# Multiple Examples
+
+Xs = np.array([[2, 0, -7],
+              [-2, 9, 0],
+              [5, 1, -1],
+              [3, 0, -4]])
+
+def multiple_neurons(Xs):    # multiple input features, multiple examples
+    w = np.array([[5, -2, -10],    # neuron 1's weights
+                  [0, 9, -5]])    # neuron 2's weights
+    b = np.array([-10, 7])    # 2 biases, one for each neuron, vector
+    return np.dot(Xs, w.T) + b  
+
+print(multiple_neurons(Xs))
+
 print("\n")
