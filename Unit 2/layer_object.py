d18061542477cfc4646170b764a2f2a938af9e15
Lukas Cao <lukascao06@gmail.com>
2021-12-02 19:47:35 -0800

    Update layer_object.py

--- Unit 2/layer_object.py
+++ Unit 2/layer_object.py
@@ -6,4 +6,15 @@ class Dense_Layer:
         self.weights = 0.01 * np.random.randn(n_neurons, n_inputs)
         self.biases = np.zeros([1, n_neurons])
 
+# =========================================================
+# Updated Layer with Prediction (forward) Method
+
+class Dense_Layer:
+    def __init__(self, n_inputs, n_neurons):
+        self.weights = 0.01 * np.random.randn(n_inputs, n_neurons)
+        self.biases = np.zeros([1, n_neurons])
+
+    def forward(self, X):
+        self.outputs = np.dot(X, self.weights) + self.biases
+
 print("\n")
