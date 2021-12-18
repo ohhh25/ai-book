a9778528d4829ce50177aea2d29deed9f2aa68aa
Lukas Cao <lukascao06@gmail.com>
2021-12-17 18:18:12 -0800

    Update cost_functions.py

--- Unit 3/cost_functions.py
+++ Unit 3/cost_functions.py
@@ -24,6 +24,7 @@ X = np.array([[0], [1], [2], [3], [4], [5], [6], [7], [8]])    # one input featu
 # percentage
 y = np.array([[20], [30], [40], [50], [60], [70], [80], [90], [100]])    # one output feature for each example
 
+# define cost functions
 mae = MAE_Cost()
 mse = MSE_Cost()
 
