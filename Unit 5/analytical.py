0b02bc960649b493ed4ab93b3e96cf33a86fbad6
Lukas Cao <lukascao06@gmail.com>
2021-12-27 22:55:05 -0800

    Update analytical.py

--- Unit 5/analytical.py
+++ Unit 5/analytical.py
@@ -9,21 +9,26 @@ class Dense_Layer:
         self.biases = np.zeros([1, n_neurons])
 
     def forward(self, inputs):    # inputs is X
+        self.inputs = inputs
         self.outputs = np.dot(inputs, self.weights) + self.biases
+
+    def backward(self, dvalues):
+        self.dweights = np.dot(self.inputs.T, dvalues)
+        self.dbiases = np.sum(dvalues, axis=0, keepdims=True)
 
 class MAE_Cost:
     def forward(self, y_pred, y_true):    # y_pred is prediction from model, y_true is the answer
         return np.mean(np.abs(y_pred - y_true))
 
     def backward(self, y_pred, y_true):
-        return np.sign(y_pred - y_true) / y_pred.size
+        self.dinputs = np.sign(y_pred - y_true) / y_pred.size
 
 class MSE_Cost:
     def forward(self, y_pred, y_true):
         return np.mean(np.square(y_pred - y_true))
 
     def backward(self, y_pred, y_true):
-        return (2 / y_pred.size) * (y_pred - y_true)
+        self.dinputs = (2 / y_pred.size) * (y_pred - y_true)
 
 # hours studied
 X = np.array([[0], [1], [2], [3], [4], [5], [6], [7], [8]])    # one input feature for each example
@@ -34,5 +39,10 @@ mse = MSE_Cost()    # define cost function
 model = Dense_Layer(1, 1)    # 1 input feature, 1 neuron (output feature)
 model.forward(X)
 cost = mse.forward(model.outputs, y)
+
+mse.backward(model.outputs, y)
+model.backward(mse.dinputs)
+print("Slope of w:", model.dweights)
+print("Slope of b:", model.dbiases)
 
 print("\n")
