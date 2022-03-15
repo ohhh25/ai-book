8d9ff2b4d1f6e8108c700f17c255401294f35b0f
Lukas Cao <lukascao06@gmail.com>
2022-03-14 19:51:59 -0700

    Update multiple_features.py

--- Unit 8/standardization/multiple_features.py
+++ Unit 8/standardization/multiple_features.py
@@ -3,7 +3,7 @@ print("\n")
 import numpy as np
 np.random.seed(1)    # for repeatability
 
-def standarize(X):
+def standardize(X):
     mean_of_zero = X - np.mean(X, axis=0)    # bringing data to have mean of 0
     X = mean_of_zero / np.std(X, axis=0)    # brinding data to have standard deviation of 1
     return X
@@ -12,7 +12,7 @@ X = np.random.uniform(low=-10, high=20, size=[4, 3])
 print(X) 
 print(np.mean(X, axis=0), np.std(X, axis=0))
 
-X = standarize(X)
+X = standardize(X)
 print("\n", X)
 print(np.mean(X, axis=0), np.std(X, axis=0))
 print(X.min(axis=0), X.max(axis=0))
