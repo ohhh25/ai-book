5b11108059841fba1f5a6966b56a804cfbf962a5
Lukas Cao <lukascao06@gmail.com>
2022-03-27 13:33:24 -0700

    Create relu_activation.py

---
+++ Unit 9/relu_activation.py
@@ -0,0 +1,14 @@
+print("\n")
+
+import numpy as np    # version 1.22.2
+
+class ReLU_Activation:
+    def forward(self, inputs):    # inputs are outputs from dense layer
+        self.inputs = inputs    # save inputs for backward method
+        self.outputs = np.maximum(0, inputs)
+
+    def backward(self, dvalues):    # dvalues has same shape as inputs
+        self.dinputs = dvalues * (self.inputs >= 0)
+
+
+print("\n")
