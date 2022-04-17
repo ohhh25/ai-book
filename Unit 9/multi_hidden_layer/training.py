f4abe60fbb7e0222e282894da9c80a7a7444bbbc
Lukas Cao <lukascao06@gmail.com>
2022-04-17 15:17:39 -0700

    Update training.py

--- Unit 9/multi_hidden_layer/training.py
+++ Unit 9/multi_hidden_layer/training.py
@@ -124,7 +124,7 @@ for epochs in range(800):
 
     # check for dead neurons
     if epochs % 80 == 0:
-        # number & percentage of dead neurons
+        # cost & percentage of dead neurons
         print(f"Cost: {model.cost} - Dead1: {get_dead(model.hidden_layer1)}% - " + \
             f"Dead2: {get_dead(model.hidden_layer2)}%")
 
