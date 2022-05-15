4a37d6dd427a4cdeda84b67f46d75fa09e7c236e
Lukas Cao <lukascao06@gmail.com>
2022-05-15 13:36:09 -0700

    Create sigmoid.py

---
+++ Unit 10/sigmoid.py
@@ -0,0 +1,19 @@
+import matplotlib.pyplot as plt    # version 3.4.0
+import numpy as np    # version 1.22.2
+import pandas as pd    # version 1.4.0
+np.random.seed(0)    # For repeatability
+
+class Sigmoid_Activation:
+    def forward(self, inputs):    # inputs are outputs from dense layer
+        self.inputs = inputs    # save inputs for backward method
+        self.outputs = 1 / (1 + np.exp(-inputs))
+        
+    def backward(self, dvalues):    # dvalues has same shape as inputs
+        self.dinputs = dvalues * (self.outputs * (1 - self.outputs))
+
+random_data = np.random.randn(10, 1)
+print(random_data)
+
+activation_function = Sigmoid_Activation()
+activation_function.forward(random_data)
+print("\n", activation_function.outputs)
