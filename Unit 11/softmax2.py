4e298bcab7bf26e63a806cc2a1527ad5fe4db48d
Lukas Cao <lukascao06@gmail.com>
2022-07-18 12:45:46 -0700

    Create softmax2.py

---
+++ Unit 11/softmax2.py
@@ -0,0 +1,20 @@
+print("\n")
+
+import numpy as np    # version 1.22.2
+np.random.seed(0)    # For repeatability
+
+class Softmax_Activation:
+    def forward(self, inputs):
+        exponentiated = np.exp(inputs - np.max(inputs, axis=1, keepdims=True))
+        self.output = exponentiated / np.sum(exponentiated, axis=1, keepdims=True)
+
+
+y_hat = np.array([[-2, 2, 5]])
+softmax = Softmax_Activation()
+softmax.forward(y_hat)
+percentages = softmax.output * 100    # convert to percentage
+print(percentages)
+print(np.sum(percentages, axis=1))    # should be 100%
+
+
+print("\n")
