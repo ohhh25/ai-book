20f9487fca8b400a8e3277bf0bb4e72e97acf435
Lukas Cao <lukascao06@gmail.com>
2022-07-30 11:02:50 -0700

    Update one_neuron_one_input_feature.py

--- Chapter 01/Code/one_neuron_one_input_feature.py
+++ Chapter 01/Code/one_neuron_one_input_feature.py
@@ -11,6 +11,23 @@ print(one_neuron(X))
 # =========================================================
 # Multiple Examples
 
+person1_X = 2
+person2_X = -2
+person3_X = 5
+person4_X = 3
+def one_neuron(Xs):    # one input feature, multiple examples
+    w = 5
+    b = -10
+    return (w * Xs) + b
+
+print(one_neuron(person1_X))
+print(one_neuron(person2_X))
+print(one_neuron(person3_X))
+print(one_neuron(person4_X))
+
+# =========================================================
+# Multiple Examples (Vectorized)
+
 import numpy as np
 Xs = np.array([2, -2, 5, 3])    # 4 examples, vector
 def one_neuron(Xs):    # one input feature, multiple examples
