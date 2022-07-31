948b2cfa2f229456271189666264cf2390702639
Lukas Cao <lukascao06@gmail.com>
2022-07-31 14:14:51 -0700

    Update layer_object.py

--- Chapter 01/Code/layer_object.py
+++ Chapter 01/Code/layer_object.py
@@ -3,7 +3,7 @@ print("\n")
 import numpy as np
 class Dense_Layer:
     def __init__(self, n_inputs, n_neurons):
-        self.weights = 0.01 * np.random.randn(n_neurons, n_inputs)
+        self.weights = 0.01 * np.random.randn(n_inputs, n_neurons)
         self.biases = np.zeros([1, n_neurons])
 
 # =========================================================
